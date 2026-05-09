777c19eaaa56f19af594b48e1c6b65f418960fbc
Huw Davies <me@huwbert.co.uk>
2026-05-09 20:29:56 +0100

    Move regression cases into existing import/moved/removed test tables
    Drop get_value_test.go in favor of adding the index-followed-by-attrs
    cases (string and number index) directly to the existing tables, per
    maintainer request.

--- removed_test.go
+++ removed_test.go
@@ -140,6 +140,42 @@ resource "null_resource" "bbb" {}
 `),
 			wantErr: false,
 		},
+		{
+			name:   "string index followed by attrs",
+			fields: fields{},
+			args: args{
+				data: []byte(`
+resource "null_resource" "aaa" {}
+removed {
+  from = module.foo["hoge"].bar.baz
+  lifecycle {
+    destroy = false
+  }
+}
+resource "null_resource" "bbb" {}
+`),
+			},
+			want:    []byte("\nresource \"null_resource\" \"aaa\" {}\nresource \"null_resource\" \"bbb\" {}\n"),
+			wantErr: false,
+		},
+		{
+			name:   "number index followed by attrs",
+			fields: fields{},
+			args: args{
+				data: []byte(`
+resource "null_resource" "aaa" {}
+removed {
+  from = module.foo[0].bar.baz
+  lifecycle {
+    destroy = false
+  }
+}
+resource "null_resource" "bbb" {}
+`),
+			},
+			want:    []byte("\nresource \"null_resource\" \"aaa\" {}\nresource \"null_resource\" \"bbb\" {}\n"),
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
